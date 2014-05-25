7503bea592c6f84e94b92c46bb857a1968736cdf
Matthew Brett <matthew.brett@gmail.com>
2014-05-25 15:34:13 -0700

    Script takes into account version numbers
    I hadn't noticed there were multiple versions of each file.  Take this
    into account explicitly.  In fact it ends up doing the same final copy
    so the website files don't change compared to running the previous
    version of the script.

--- scripts/deversion.py
+++ scripts/deversion.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """ Remove file-name versioning now we're in git """
+from __future__ import print_function
 
 from os.path import join as pjoin, split as psplit, abspath, dirname
 import os
@@ -12,10 +13,11 @@ href_re = re.compile(r'<a href="(\w+)/(\w+)_\dp\d.pdf">')
 EXAMPLE = '<a href="decretum/idecforw_1p4.pdf">'
 EXAMPLE2 = '<a href="decretum/leiden_1p4.pdf">'
 
-print href_re.sub(r'<a href="\1/\2.pdf">', EXAMPLE)
-print href_re.match(EXAMPLE2).groups()
+print(href_re.sub(r'<a href="\1/\2.pdf">', EXAMPLE))
+print(href_re.match(EXAMPLE2).groups())
 
 for dirpath, dirnames, filenames in os.walk('.'):
+    fname_matches = {}
     for fname in filenames:
         fpath = pjoin(dirpath, fname)
         if fname.endswith('.html'):
@@ -28,5 +30,16 @@ for dirpath, dirnames, filenames in os.walk('.'):
             continue
         if path_re.match(fname):
             new_fname = path_re.sub(r'\1.pdf', fname)
-            new_fpath = pjoin(dirpath, new_fname)
-            shutil.copyfile(fpath, new_fpath)
+            if new_fname in fname_matches:
+                fname_matches[new_fname].append(fname)
+            else:
+                fname_matches[new_fname] = [fname]
+    for new_fname, matches in fname_matches.items():
+        matches.sort()
+        print("Checking", new_fname)
+        print("Matches are:")
+        print('\n'.join(matches))
+        old_fpath = pjoin(dirpath, matches[-1])
+        new_fpath = pjoin(dirpath, new_fname)
+        print("Copying {0} as {1}".format(old_fpath, new_fpath))
+        shutil.copyfile(old_fpath, new_fpath)
